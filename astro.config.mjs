01e83e747dccac10636647a482e1eb9268945003
Nicolas <nicolas.herissard@gmail.com>
2024-06-02 14:29:23 +0200

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  base: 'https://github.com/NicolasHerissard/NicolasHerissard.github.io.git',
-  outDir: 'dist',
+  integrations: [tailwind()]
 });
