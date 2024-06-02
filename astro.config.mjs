1cf94402d94cbf5705025ac16d4e15970efd7202
Nicolas <nicolas.herissard@gmail.com>
2024-06-02 14:43:11 +0200

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const GITHUB_URL = "https://NicolasHerissard.github.io.git"
+const GITHUB_REPO = "https://github.com/NicolasHerissard/NicolasHerissard.github.io.git"
+
 // https://astro.build/config
 export default defineConfig({
+  site: GITHUB_URL,
   integrations: [tailwind()]
 });
