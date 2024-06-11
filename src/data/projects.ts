cc3f7ccbb9b824e3b1e2c590ec65bf138a1d5c59
Nicolas <nicolas.herissard@gmail.com>
2024-06-11 23:27:40 +0200

    add project list and design card project

---
+++ src/data/projects.ts
@@ -0,0 +1,30 @@
+export const projectsLists = [
+    {
+        name: "Portfolio",
+        description: "Code source de ce site web",
+        date: "2024",
+        tech: ["Astro", "TypeScript", "Tailwind CSS", "HTML"],
+        url: "https://github.com/NicolasHerissard/NicolasHerissard.github.io"
+    },
+    {
+        name: "Telematos",
+        description: "Application web de gestion de matériel pour le télétravail",
+        date: "2023",
+        tech: ["PHP", "Laravel", "SQL"],
+        url: "https://github.com/NicolasHerissard/Telematos"
+    },
+    {
+        name: "Moulagapp",
+        description: "Gestion de compte bancaire",
+        date: "2024",
+        tech: ["Java", "JavaFX", "SQL"],
+        url: "https://github.com/NicolasHerissard/Moulagapp"
+    },
+    {
+        name: "api-telematos",
+        description: "API pour l'application web de gestion de matériel pour le télétravail",
+        date: "2024",
+        tech: ["Go", "Gin", "SQL"],
+        url: "https://github.com/NicolasHerissard/api-telematos"
+    },
+]
