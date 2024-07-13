2484cbc182b4738b46459dafbb2e0c5e5ed245f4
Nicolas <nicolas.herissard@gmail.com>
2024-07-13 22:43:19 +0200

    modif commit bat file

--- commit.bat
+++ commit.bat
@@ -1,5 +1,6 @@
 @echo off
 
+git init
 git status
 git add .
 
@@ -7,7 +8,3 @@ set /p comment=Entrez un commentaire:
 git commit -m "%comment%"
 
 git push origin master
-
-echo Le projet à été enregistré sur Github
-
-pause
